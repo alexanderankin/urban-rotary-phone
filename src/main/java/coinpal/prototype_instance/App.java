5ca5c489d7daa9c21e03fba3b3c0f3ed885150bd
Dave Ankin <alexanderankin@gmail.com>
2017-10-01 03:02:09 -0400

    changed project to java8 and wrote example test

---
+++ src/main/java/coinpal/prototype_instance/App.java
@@ -0,0 +1,15 @@
+package coinpal.prototype_instance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+	public boolean isAwesome() {
+		return true;
+	}
+}
