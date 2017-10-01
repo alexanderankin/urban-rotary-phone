be76f3070152e495dda888dfb5204dfc5e53a0de
Dave Ankin <alexanderankin@gmail.com>
2017-10-01 04:59:45 -0400

    junit 5 + relearned strings

--- src/main/java/coinpal/prototype_instance/structures/Address.java
+++ src/main/java/coinpal/prototype_instance/structures/Address.java
@@ -1,24 +1,29 @@
 package coinpal.prototype_instance.structures;
 
 public class Address {
+	public static final int MAX_LENGTH = 35;
+	public static final int MIN_LENGTH = 26;
 	private final char[] address;
+
 	public Address() {
 		throw new java.lang.UnsupportedOperationException();
 	}
-	
-	public Address(String address) {
-		this.address = new char[35];
 
-		charifyAddress(address);
-	}
-	
-	private void charifyAddress(String address) {
-		int index = 0;
-		for (; index < address.length(); index++) {
-			this.address[index] = address.charAt(index);
+	public Address(String address) {
+		int length = address.length();
+
+		if (length >= MAX_LENGTH) {
+			throw new IllegalArgumentException("Address is too long: " + address);
 		}
+
+		if (length <= MIN_LENGTH) {
+			throw new IllegalArgumentException("Address is too short: " + address);
+		}
+
+		this.address = new char[length];
+		System.arraycopy(address.toCharArray(), 0, this.address, 0, this.address.length);
 	}
-	
+
 	@Override
 	public String toString() {
 		return new String(address);
--- src/test/java/coinpal/prototype_instance/structures/AddressTest.java
+++ src/test/java/coinpal/prototype_instance/structures/AddressTest.java
@@ -1,10 +1,38 @@
 package coinpal.prototype_instance.structures;
 
-import junit.framework.TestCase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class AddressTest extends TestCase {
-	public void testAddress() {
-		Address a = new Address("abc");
-		System.out.println(a.toString());
+import org.junit.jupiter.api.Test;
+
+class AddressTest {
+
+	@Test
+	void instantiateFromString() {
+		String sample = "1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2";
+		Address a = new Address(sample);
+		assertEquals(sample, a.toString());
+	}
+
+	@Test
+	void throwAddressWrongLength() {
+		String tooShort = "abc";
+		Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+			new Address(tooShort);
+		});
+		assertEquals("Address is too short: " + tooShort, exception.getMessage());
+
+		String tooLong = "abcdefghijklmnopqrstuvxyzabcdefghijkl";
+		exception = assertThrows(IllegalArgumentException.class, () -> {
+			new Address(tooLong);
+		});
+		assertEquals("Address is too long: " + tooLong, exception.getMessage());
+	}
+
+	@Test
+	void throwNoAddress() {
+		assertThrows(UnsupportedOperationException.class, () -> {
+			new Address();
+		});
 	}
 }
