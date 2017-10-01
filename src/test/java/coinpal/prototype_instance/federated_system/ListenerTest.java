ae5c3892e489df444dfbf62ac634f0335f39708b
Dave Ankin <alexanderankin@gmail.com>
2017-10-01 10:35:28 -0400

    tested logger

--- src/test/java/coinpal/prototype_instance/federated_system/ListenerTest.java
+++ src/test/java/coinpal/prototype_instance/federated_system/ListenerTest.java
@@ -1,25 +1,39 @@
 package coinpal.prototype_instance.federated_system;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.junit.jupiter.api.Test;
 
 import coinpal.prototype_instance.events.EventStream;
 import coinpal.prototype_instance.events.Ping;
 import coinpal.prototype_instance.events.Request;
 import coinpal.prototype_instance.events.Response;
+import mockit.Mocked;
+import mockit.Verifications;
 
 class ListenerTest {
 
+	@Mocked
+	Logger logger;
+
 	@Test
-	void logsOnNewRequest() throws InterruptedException {
+	void logsOnNewRequest() {
 		EventStream<Request> requests = new EventStream<Request>();
 		EventStream<Response> responses = new EventStream<Response>();
 		FederatedSystem system = new FederatedSystem();
 		Listener listener = new Listener(requests, responses, system);
 		Thread thread = new Thread(listener);
 		thread.start();
-		requests.put(new Ping(0));
-		Thread.currentThread().sleep(1000);
-		thread.interrupt();
+		Request ping = new Ping(0);
+		requests.add(ping); // FIXME use put
+
+		new Verifications() {
+			{
+				logger.log(Level.INFO, "Processing Request " + ping.toString());
+				times = 1;
+			}
+		};
 	}
 
 }
